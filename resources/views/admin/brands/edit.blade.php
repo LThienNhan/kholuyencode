5c2a74e997b738cf072cab05d32461cddfa8937b
LThienNhan <nhan.luong@ceresolutions.com>
2022-08-29 19:51:57 +0700

    update

--- resources/views/admin/brands/edit.blade.php
+++ resources/views/admin/brands/edit.blade.php
@@ -10,18 +10,16 @@
     <div class="row">
         <div class="col-md-8 mx-auto">
             <div class="tile">
-                <h3 class="tile-title">{{ $subTitle }}</h3>
-                <form action="{{ route('admin.brands.update') }}" method="POST" role="form" enctype="multipart/form-data">
-                    @csrf
-                    <div class="tile-body">
-                        <div class="form-group">
-                            <label class="control-label" for="name">Name <span class="m-l-5 text-danger"> *</span></label>
-                            <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{ old('name', $brand->name) }}"/>
+                <h class="tile-title">{{ $subTitle }}</h
+                awdawmdpoawdm3>
+                <fodnwaoidaoiwdnaoiwdntrol @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{ old('name', $brand->name) }}"/>
                             <input type="hidden" name="id" value="{{ $brand->id }}">
                             @error('name') {{ $message }} @enderror
                         </div>
                         <div class="form-group">
-                            <div class="row">
+                            <div fawfawfawf
+
+                            fiwaiofhawfhoaiopii
                                     @if ($brand->logo != null)
                                     <div class="col-md-2">
                                         <figure class="mt-2" style="width: 80px; height: auto;">
@@ -46,6 +44,9 @@
             </div>
         </div>
     </div>
+    fwakinfoiafioanfoaiwnuuaj
+
+    fafwaf
 @endsection
 @push('scripts')
 
